f9b955b4ad66130e05ceace061c4277ee524d304
wudi-ubuntu <1582074055@qq.com>
2022-11-16 20:49:39 +0800

    add  everything

---
+++ examples/hello/hello-decode/build/CMakeFiles/hello-decode.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wudi/Desktop/hellodecode/hello-decode/examples/hello/hello-decode/src/hello-decode.cpp" "/home/wudi/Desktop/hellodecode/hello-decode/examples/hello/hello-decode/build/CMakeFiles/hello-decode.dir/src/hello-decode.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LIBVA_SUPPORT"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/home/wudi/intel/oneapi/vpl/2022.1.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ examples/hello/hello-decode/build/CMakeFiles/hello-decode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello-decode.dir/src/hello-decode.cpp.o"
+  "hello-decode.pdb"
+  "hello-decode"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello-decode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
